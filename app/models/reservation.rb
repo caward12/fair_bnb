9c46057d46fa853c9b87f9789ef45efba6b306e0
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-05-23 01:44:02 -0600

    add formatting methods for reservation index

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,33 @@
 class Reservation < ApplicationRecord
   validates :total_price, :start_date, :end_date, :number_of_guests, :status, presence: true
-  
+
   belongs_to :property
   belongs_to :renter, class_name: "User", foreign_key: "renter_id"
 
   enum status: %w(pending confirmed in_progress finished declined)
+
+  def city
+    property.city
+  end
+
+  def check_in_date
+    start_date.strftime("%b %d, %Y")
+  end
+
+  def check_out_date
+    end_date.strftime("%b %d, %Y")
+  end
+
+  def guests
+    pluralize(number_of_guests, 'guest')
+  end
+
+  def title
+    property.name
+  end
+
+  def image_url
+    property.image_url
+  end
+
 end
