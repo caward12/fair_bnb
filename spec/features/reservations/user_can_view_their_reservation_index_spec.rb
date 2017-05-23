10382a17fde253a8c2778d26af95c23027c2ed0c
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-05-23 01:46:14 -0600

    write feature tests for users accessing my reservations

--- spec/features/reservations/user_can_view_their_reservation_index_spec.rb
+++ spec/features/reservations/user_can_view_their_reservation_index_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 feature "user reservation index page" do
+  attr_reader :user, :user2
+  before :each do
+    @user = create(:user_with_reservations)
+    @user2 = create(:user_with_reservations)
+  end
   context "as a guest" do
     scenario "guest should not be able to navigate to my reservations" do
       visit root_path
@@ -10,23 +15,14 @@ feature "user reservation index page" do
     scenario "guest should receive a 404" do
       visit user_reservations_path
 
-      expect(response.status).to eq(404)
+      expect(page.status_code).to eq(404)
 
-      within '.alert-error' do
-        page.should have_content("Page page_not_found doesn't exist")
+      within '.alert' do
+        expect(page).to have_content("Page not found!")
       end
     end
   end
   context "as a logged in user" do
-    attr_reader :user, :pending, :confirmed, :in_progress, :finished, :declined
-    before :each do
-      user = create(:user_with_reservations)
-      pending = user.reservations.find_by(status: 0)
-      confirmed = user.reservations.find_by(status: 1)
-      in_progress = user.reservations.find_by(status: 2)
-      finished = user.reservations.find_by(status: 3)
-      declined = user.reservations.find_by(status: 4)
-    end
     scenario "should be able to navigate to my reservations" do
       login(user)
 
@@ -35,18 +31,42 @@ feature "user reservation index page" do
       click_link "My Reservations"
 
       expect(current_path).to eq(user_reservations_path)
+
+      logout
     end
     scenario "return a collection of my reservations" do
       login(user)
       visit user_reservations_path
 
-      within(".pending") do
-        expect(page).to have_content()
+      save_and_open_page
+
+      %w(pending confirmed in_progress finished declined).each do |status|
+        within(".#{status}") do
+          reservation = user.reservations.find_by(status: status)
+          expect(page).to have_selector('.reservation', count: 1)
+          expect(page).to have_css("img[src*='#{reservation.property.image_url}']")
+          expect(page).to have_link(reservation.property.city)
+          expect(page).to have_content(reservation.title)
+          expect(page).to have_content(reservation.check_in_date)
+          expect(page).to have_content(reservation.check_out_date)
+          expect(page).to have_content("#{reservation.number_of_guests} guest")
+        end
       end
 
+      logout
     end
     scenario "i cannot navigate to another user's reservations" do
+      login(user2)
+      visit user_reservations_path
 
+      %w(pending confirmed in_progress finished declined).each do |status|
+        within(".#{status}") do
+          reservation = user.reservations.find_by(status: status)
+          expect(page).to_not have_css("img[src*='#{reservation.property.image_url}']")
+          expect(page).to_not have_link(reservation.property.city)
+          expect(page).to_not have_content(reservation.title)
+        end
+      end
     end
   end
 end
