b7352c89dda30609232c3cb849c8971112f32ae8
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-05-24 09:32:55 -0600

    outline reservation feature tests

--- spec/features/reservations/user_can_make_reservation_spec.rb
+++ spec/features/reservations/user_can_make_reservation_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-feature "guests ca" do
+feature "request to book" do
+  context "as a guest user" do
+    scenario "prompted to login when attempting to book" do
 
+    end
+    scenario "once logged in, i am redirected back to the reservation booking page" do
+
+    end
+  end
+  context "as a logged in user" do
+    scenario "directed to booking page" do
+
+    end
+    scenario "pending booking shows up "
+  end
 end
