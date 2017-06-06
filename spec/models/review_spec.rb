0196f3a015ebf4c3ed1a7cce31e6c5e8219c88f6
bschwartz10 <bschwartz92@gmail.com>
2017-06-05 18:01:28 -0600

    add validation tests for reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "review is valid with all attributes" do
+    before do
+      @user = create(:user)
+      @property = create(:property)
+    end
+    it { should validate_presence_of(:rating) }
+    it { should validate_presence_of(:comment) }
+    it { should belong_to :user }
+    it { should belong_to :property }
+  end
+
+  it "is invalid without rating" do
+    review = build(:review, rating: nil, comment: "Great Home!", user: @user, property: @property)
+    expect(review).to_not be_valid
+  end
+
+  it "is invalid without comment" do
+    review = build(:review, rating: 5, comment: nil, user: @user, property: @property)
+    expect(review).to_not be_valid
+  end
+
+  it "is invalid without owner" do
+    review = build(:review, rating: 5, comment: "Great Home", user: nil, property: @property)
+    expect(review).to_not be_valid
+  end
+
+  it "is invalid without property" do
+    review = build(:review, rating: 5, comment: "Great Home", user: @user, property: nil)
+    expect(review).to_not be_valid
+  end
+
 end
