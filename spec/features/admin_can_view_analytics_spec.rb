bccd21856efa4f3e6377f07f1381e92a3d9cc7c1
caward12 <ward.colleen.a@gmail.com>
2017-06-06 16:29:17 -0600

    update test for analytics page

--- spec/features/admin_can_view_analytics_spec.rb
+++ spec/features/admin_can_view_analytics_spec.rb
@@ -2,7 +2,20 @@ require 'rails_helper'
 
 RSpec.feature "as an admin " do
   scenario "i can see analytics page" do
+    admin = create(:user, role: 1)
 
-    
+    login(admin)
+
+    visit admin_analytics_path
+
+    expect(page).to have_content("Number of Reservations by Month")
+    expect(page).to have_css("#monthContainer")
+    expect(page).to have_content("Reservation Revenue by Month")
+    expect(page).to have_css("#chartContainer")
+    expect(page).to have_content("Properties by State")
+    expect(page).to have_css("#mapContainer")
+    expect(page).to have_css("#tooltip")
+
+
   end
 end
