2240a183ebf01a6a0b0d87fcddb5a2e911a68371
bschwartz10 <bschwartz92@gmail.com>
2017-06-06 12:13:06 -0600

    WIP add js logic to create reviews action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,19 @@
 class ReviewsController < ApplicationController
 
   def create
-    property = Property.find(params[:property_id])
-    review = property.reviews.new(review_params)
-    review.update(user_id: current_user.id)
-    redirect_to property_path(property)
+    @property = Property.find(params[:property_id])
+    @review = @property.reviews.new(review_params)
+
+    respond_to do |format|
+      if @review.update(user_id: current_user.id)
+        format.html { redirect_to property_path(@property), notice: 'Review was successfully created.' }
+        format.js   { }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
@@ -12,4 +21,5 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :comment)
   end
+
 end
