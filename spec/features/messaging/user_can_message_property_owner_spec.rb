9740cb5fbf52ed7a51a58ff0db264733ac628547
Sam Landfried <samlandfried@gmail.com>
2017-06-06 18:51:37 -0600

    Test fetching/creating conversations on show page

--- spec/features/messaging/user_can_message_property_owner_spec.rb
+++ spec/features/messaging/user_can_message_property_owner_spec.rb
@@ -1,16 +1,49 @@
 require 'rails_helper'
 
 RSpec.feature "Registered user can message a property owner" do
-  scenario "and it starts a new conversation" do
-    user = create(:user)
-    property = create(:property)
-    user2 = property.owner
+  attr_reader :user, :property, :owner
+
+  before do
+    @user = create(:user)
+    @property = create(:property)
+    @owner = property.owner
+
     login(user)
-    
+
     visit property_path(property)
-    click_on "Message #{user2.first_name}"
-    
-    expect(current_path).to eq(conversation_path(Conversation.first))
-    expect(page).to have_content("Chat between")
+  end
+
+  context 'And a conversation does not exist' do
+    scenario "So it starts a new conversation" do
+      click_on "Message #{owner.first_name}"
+
+      convo = Conversation.find_by(user: user, property: property)
+
+      expect(current_path).to eq(conversation_path(convo))
+      expect(page).to have_content("Chat between #{user.first_name} and #{owner.first_name} about #{property.name}")
+      expect(page).to have_link property.name
+
+      expect(page).to have_selector('.conversation-messages:empty')
+    end
+  end
+
+  context 'And a conversation exists' do
+    scenario 'So it resumes the old conversation' do
+      convo = create :conversation, property: property, user: user
+      message = convo.messages.first
+
+      click_on "Message #{owner.first_name}"
+
+      expect(current_path).to eq(conversation_path(convo))
+      expect(page).to have_content("Chat between #{user.first_name} and #{owner.first_name} about #{property.name}")
+      expect(page).to have_link property.name
+
+      within '.conversation-messages' do
+        within '.message:first' do
+          expect(page).to have_content message.body
+          expect(page).to have_content message.author
+        end
+      end
+    end
   end
 end
