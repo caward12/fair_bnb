c9821d13a86b1b62f8de44739d0968952c220f21
Sam Landfried <samlandfried@gmail.com>
2017-06-07 11:37:12 -0600

    Implement ChatterMessage model

--- spec/models/chatter_message_spec.rb
+++ spec/models/chatter_message_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe ChatterMessage do
   describe 'Attributes' do
-    attr_reader :cm
+    attr_reader :cm, :user
 
     before do
-      user = create :user
+      @user = create :user
 
       params = {
         body: 'Im a ChatterMessage body',
         author: user.id
       }
 
-      @cm = ChatterMessage.new(cm)
+      @cm = ChatterMessage.new(params)
     end
 
     it 'Has a body' do
       expect(cm.body).to eq 'Im a ChatterMessage body'
     end
 
-    it 'Has an author' do
-      expect(cm.user).to be user
+    it 'Has a user' do
+      expect(cm.user).to eq user
     end
   end
 end
