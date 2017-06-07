fa83fa0c9c3ecd51e8848d53b37aeba56c1a2e3e
Riley Thompson <rileyt_2000@hotmail.com>
2017-06-07 15:43:35 -0600

    change admin dropdown

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,7 +7,6 @@ class Property < ApplicationRecord
   scope :pending_properties, -> { where(status: 0) }
   belongs_to :room_type
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
-
   has_many :reservations
   has_many :property_availabilities
 
