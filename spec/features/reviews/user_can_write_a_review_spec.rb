35a3557255e04473fb7c5ec0b9a2bc18a4d5b8ac
bschwartz10 <bschwartz92@gmail.com>
2017-06-07 16:15:43 -0600

    edit test to check for css #new_review

--- spec/features/reviews/user_can_write_a_review_spec.rb
+++ spec/features/reviews/user_can_write_a_review_spec.rb
@@ -5,27 +5,11 @@ RSpec.feature "as a user I can write a review for a property" do
     @property = create(:property)
     @user = create(:user)
   end
-  xscenario "when I click on the reviews tab I see a field to rate a property 1-5 and a text field to write a review" do
-
-    visit property_path(@property)
-
-    click_on "Reviews"
-    expect(page).to have_field("review[rating]", visible: false)
-    expect(page).to have_field("review[comment]")
-  end
-
-  xscenario "i fill out form fields and create a review" do
+  scenario "when I click on the reviews tab I see a form to write a review" do
     login(@user)
-
     visit property_path(@property)
-    click_on "Reviews"
-    # fill_in "review[rating]", visible: false, with: 5
-    # first("#star-rating", :visible => false).set(5)
-    fill_in "review[comment]", with: "Great Home"
-    click_on "Submit"
 
-    expect(Review.last.rating).to eq(5)
-    expect(Review.last.comment).to eq("Great Home")
-    expect(current_path).to eq(property_path(@property))
+    click_on "Reviews"
+    expect(page).to have_css("#new_review")
   end
 end
