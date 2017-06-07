23f9a6aa076f2530631307d28e1276227cfe52ce
bschwartz10 <bschwartz92@gmail.com>
2017-06-07 16:03:32 -0600

    paginate reviews

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,6 +4,7 @@ class PropertiesController < ApplicationController
     @property = Property.find(params[:id])
     @weather = @property.get_weather
     @review = Review.new
+    @property_reviews = @property.reviews.order(:created_at).paginate(:per_page => 3, :page => params[:page])
   end
 
   def index
