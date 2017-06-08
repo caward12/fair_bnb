80af2b08886e29ee10f2111923ec5e4b84fb908f
bschwartz10 <bschwartz92@gmail.com>
2017-06-07 19:27:58 -0600

    add precompile commands

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,8 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( analytics.js )
+Rails.application.config.assets.precompile += %w( jquery.raty.js )
+Rails.application.config.assets.precompile += %w( ratings.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
